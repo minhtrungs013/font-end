f1f6a26dc0917dc6d874d0e874804d638f840324
đỗ minh trung <56585861+minhtrungs013@users.noreply.github.com>
2021-10-16 00:00:32 +0700

    hi.java
    đổi main

--- hi.java
+++ hi.java
@@ -1,4 +1,6 @@
 class chao(){
 	public static void main(string args[0])
 		system.out.println("chao :", args[0]);
+	system.out.println("chao :", args[0]);
+	system.out.println("chao :", args[0]);
 }
