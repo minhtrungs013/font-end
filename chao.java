57e12f1ed75798a03245a4813929aeccd8b29962
trung <trungs0132@gmail.com>
2021-10-16 15:14:55 +0700

    trung

--- chao.java
+++ chao.java
@@ -4,3 +4,9 @@ class chao(){
 	system.out.println("chao :");
 	system.out.println("chao :");
 }
+class chao(){
+	public static void main(string args[])
+		system.out.println("chao :");
+	system.out.println("chao :");
+	system.out.println("chao :");
+}
