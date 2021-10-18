3bd651bcc7e6a8d06fd9de71d2ecca24ef19bbfd
đỗ minh trung <56585861+minhtrungs013@users.noreply.github.com>
2021-10-18 17:27:18 +0700

    Update chao.java

--- chao.java
+++ chao.java
@@ -10,3 +10,15 @@ class chao(){
 	system.out.println("chao :");
 	system.out.println("chao :");
 }
+class chao(){
+	public static void main(string args[])
+		system.out.println("chao :");
+	system.out.println("chao :");
+	system.out.println("chao :");
+}
+class chao(){
+	public static void main(string args[])
+		system.out.println("chao :");
+	system.out.println("chao :");
+	system.out.println("chao :");
+}
