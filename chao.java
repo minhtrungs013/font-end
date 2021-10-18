7d5495bef0410f0fd392087b55094f9fa88e00b4
trung <trungs0132@gmail.com>
2021-10-18 17:05:28 +0700

    tạo thêm mới

--- chao.java
+++ chao.java
@@ -2,3 +2,23 @@ class chao(){
 	public static void main(string args[])
 		system.out.println("chao :");
 }
+class chao(){
+	public static void main(string args[])
+		system.out.println("chao :");
+}
+class chao(){
+	public static void main(string args[])
+		system.out.println("chao :");
+}
+class chao(){
+	public static void main(string args[])
+		system.out.println("chao :");
+}
+class chao(){
+	public static void main(string args[])
+		system.out.println("chao :");
+}
+class chao(){
+	public static void main(string args[])
+		system.out.println("chao :");
+}
